a5b8c37c1aeff4582114711c4924253572827234
Ivan Storck <ivanoats@gmail.com>
2015-12-07 10:17:36 -0800

    unencrypt and fixes to rot13

--- class-07-functional-programming/examples/rot13.js
+++ class-07-functional-programming/examples/rot13.js
@@ -7,11 +7,11 @@ var encryptor = function(x) {
   return String.fromCharCode( x.charCodeAt(0) + 13 );
 };
 
-// String.prototype does not have the filter method
+// String.prototype does not have the map method
 // TypeError: plaintext.map is not a function
 //var result = plaintext.map(encryptor);
 
-// We can borrow includes from String
+// We can borrow map from Array
 var result = Array.prototype.map.call( plaintext, encryptor ).join('');
 
 console.log( result );
